7f2948e83b443cedd7cd5889755650e0772cb023
Emre Baser <emreeebaser@gmail.com>
2023-04-05 03:54:46 +0300

    first commit

---
+++ clock.js
@@ -0,0 +1,14 @@
+let firstName = prompt("Lütfen adınızı giriniz");
+let myName=document.querySelector("#myName")
+myName.innerHTML=firstName
+
+
+function showTime(){
+    let myClock=document.querySelector(`#myClock`)
+    let date= new Date()
+    const days=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
+    myClock.innerHTML=`
+    ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} 
+    ${days[date.getDay()]}
+    `  
+}
